c371f33df8012f0eb7c48c0b31109f607c810c73
Lucas Persson <lucas+dropbox@solrike.se>
2019-09-16 20:40:44 +0200

    update info abut projetions

--- src/main/java/se/solrike/books/model/BookView.java
+++ src/main/java/se/solrike/books/model/BookView.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 
 public interface BookView {
   
+  // as long no @Value is used the projection is closed and spring-data can optimize what is fetched from the DB
+  // but also collections forces the projection to be open which means no DB fetch optimizations.  
+  
   @JsonGetter("name")
   String getmName();
 
